c3786bfd21c42ecce646611de463ac67351e33fe
r11b22 <joost.lehaen@gmail.com>
2026-06-28 18:23:27 +0200

    Cubemap: support flip per face

--- include/Texture/CubemapLoader.hpp
+++ include/Texture/CubemapLoader.hpp
@@ -25,38 +25,43 @@ class CubmapLoader {
 template <>
 struct AssetLoadInfo<CubemapTexture> {
     std::string name;
-    std::filesystem::path pathRight;
-    std::filesystem::path pathLeft;
-    std::filesystem::path pathFront;
-    std::filesystem::path pathBack;
-    std::filesystem::path pathTop;
-    std::filesystem::path pathBottom;
-    bool flipped;
+    std::filesystem::path pathRight; bool flippedRight = false;
+    std::filesystem::path pathLeft; bool flippedLeft = false;
+    std::filesystem::path pathFront; bool flippedFront = false;
+    std::filesystem::path pathBack; bool flippedBack = false;
+    std::filesystem::path pathTop; bool flippedTop = false;
+    std::filesystem::path pathBottom; bool flippedBottom = false;
 
-    void setPath(CubeFace face, std::filesystem::path path){
+    void setPath(CubeFace face, std::filesystem::path path, bool flipped = false){
         switch (face) {
             case CubeFace::Right:
             pathRight = path;
+            flippedRight = flipped;
             break;
 
             case CubeFace::Left:
             pathLeft = path;
+            flippedLeft = flipped;
             break;
 
             case CubeFace::Front:
             pathFront = path;
+            flippedFront = flipped;
             break;
 
             case CubeFace::Back:
             pathBack = path;
+            flippedBack = flipped;
             break;
 
             case CubeFace::Top:
             pathTop = path;
+            flippedTop = flipped;
             break;
 
             case CubeFace::Bottom:
             pathBottom = path;
+            flippedBottom = flipped;
             break;
         }
     }
@@ -67,12 +72,12 @@ struct AssetLoader<CubemapTexture> {
     static CubemapTexture load(AssetLoadInfo<CubemapTexture> asset) {
         CubmapLoader loader{};
 
-        loader.readFile(CubeFace::Right,  asset.pathRight, asset.flipped);
-        loader.readFile(CubeFace::Left,   asset.pathLeft, asset.flipped);
-        loader.readFile(CubeFace::Top,    asset.pathTop, asset.flipped);
-        loader.readFile(CubeFace::Bottom, asset.pathBottom, asset.flipped);
-        loader.readFile(CubeFace::Front,  asset.pathFront, asset.flipped);
-        loader.readFile(CubeFace::Back,   asset.pathBack, asset.flipped);
+        loader.readFile(CubeFace::Right,  asset.pathRight, asset.flippedRight);
+        loader.readFile(CubeFace::Left,   asset.pathLeft, asset.flippedLeft);
+        loader.readFile(CubeFace::Top,    asset.pathTop, asset.flippedTop);
+        loader.readFile(CubeFace::Bottom, asset.pathBottom, asset.flippedBottom);
+        loader.readFile(CubeFace::Front,  asset.pathFront, asset.flippedFront);
+        loader.readFile(CubeFace::Back,   asset.pathBack, asset.flippedBack);
 
         CubemapTexture texture = loader.createCubemap(asset.name);
 
