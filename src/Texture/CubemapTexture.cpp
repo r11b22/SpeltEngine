4606cf30c6b1325ffbf5837c54fdf8f64e5ab6f7
r11b22 <joost.lehaen@gmail.com>
2026-06-29 13:53:02 +0200

    Fixed cubemap orrientation

--- src/Texture/CubemapTexture.cpp
+++ src/Texture/CubemapTexture.cpp
@@ -7,17 +7,17 @@
 GLenum toGLEnum(CubeFace face){
     switch (face) {
         case CubeFace::Right:
-        return GL_TEXTURE_CUBE_MAP_NEGATIVE_X;
-        case CubeFace::Left:
         return GL_TEXTURE_CUBE_MAP_POSITIVE_X;
+        case CubeFace::Left:
+        return GL_TEXTURE_CUBE_MAP_NEGATIVE_X;
         case CubeFace::Top:
         return GL_TEXTURE_CUBE_MAP_POSITIVE_Y;
         case CubeFace::Bottom:
         return GL_TEXTURE_CUBE_MAP_NEGATIVE_Y;
         case CubeFace::Back:
-        return GL_TEXTURE_CUBE_MAP_POSITIVE_Z;
-        case CubeFace::Front:
         return GL_TEXTURE_CUBE_MAP_NEGATIVE_Z;
+        case CubeFace::Front:
+        return GL_TEXTURE_CUBE_MAP_POSITIVE_Z;
     }
 }
 
